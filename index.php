8298231361fb44ab174a17c20bf01e6cdfd53101
ThomasBallandras <104768640+ThomasBallandras@users.noreply.github.com>
2023-01-24 08:21:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "Hello World!<br>";
 echo "<br>";
-echo "Nous sommes le " . date("d/m/Y") . ", il est " . date("H:i") . ".<br>";
+echo "Nous sommes le " . date("d/m/Y") . ", il est " . date("H:i") . " (GMT).<br>";
 ?>
